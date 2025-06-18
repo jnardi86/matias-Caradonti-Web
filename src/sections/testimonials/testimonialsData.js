93ceffdd079850a9c7315789754e87b8754766e1
jnardi86 <julian.nardi@gmail.com>
2025-06-17 22:59:36 -0300

    Change testimonialsData.js

--- src/sections/testimonials/testimonialsData.js
+++ src/sections/testimonials/testimonialsData.js
@@ -1,27 +1,27 @@
 const testimonios = [
     {
       nombre: "Sergio",
-      foto: "/gallery/testimonials/sergio.png",
+      foto: "/gallery/testimonials/Sergio.png",
       reseña: "Después de varias cirugías fallidas, hoy puedo orinar con normalidad. Gracias al Dr. Matías Caradonti y su equipo por su compromiso."
     },
     {
       nombre: "Nataly",
-      foto: "/gallery/testimonials/nataly.png",
+      foto: "/gallery/testimonials/Nataly.png",
       reseña: "Fue un camino difícil, pero hoy me siento plena. Gracias al Dr. Caradonti por acompañarme en cada paso."
     },
     {
       nombre: "Stefania",
-      foto: "/gallery/testimonials/stefania.png",
+      foto: "/gallery/testimonials/Stefania.png",
       reseña: "Cada experiencia es única. Estoy feliz de haber cumplido este paso con el Dr. Matías Caradonti y su equipo."
     },
     {
       nombre: "Johana",
-      foto: "/gallery/testimonials/johana.png",
+      foto: "/gallery/testimonials/Johana.png",
       reseña: "Fui operada de una Genitoplastia feminizante y agradezco al Dr. Matías Caradonti por su contención en todo momento."
     },
     {
       nombre: "Nicole",
-      foto: "/gallery/testimonials/nicole.png",
+      foto: "/gallery/testimonials/Nicole.png",
       reseña: "Cumplí cada indicación con compromiso y recibí lo mismo del Dr. Matías Caradonti y su equipo. Gracias por tanto."
     }
   ];
